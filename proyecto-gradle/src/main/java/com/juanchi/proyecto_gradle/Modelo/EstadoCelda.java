ec6702293a2d8cd03f229cebb142b340610c518f
juanchii-coder <71896282+juanchii-coder@users.noreply.github.com>
2024-12-28 20:28:26 -0300

    jk

---
+++ proyecto-gradle/src/main/java/com/juanchi/proyecto_gradle/Modelo/EstadoCelda.java
@@ -0,0 +1,7 @@
+package com.juanchi.proyecto_gradle.Modelo;
+
+public enum EstadoCelda {
+
+	DESCONOCIDO,RELLENO,MARCADO;
+
+}
---
+++ proyecto-gradle/src/test/java/com/juanchi/proyecto_gradle/Test.java
@@ -0,0 +1,15 @@
+package com.juanchi.proyecto_gradle;
+
+import picross.Modelo.Tablero;
+
+public class Test {
+
+	public static void main(String[] args) {
+
+		Tablero test= new Tablero(10);
+
+		System.out.print(test.toString());
+
+	}
+
+}
